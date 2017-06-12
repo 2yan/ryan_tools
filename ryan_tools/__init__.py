a206918d2e0c6e271964c37aff0ede3ea93471b2
Ryan Francis <rfrancis@graphicproducts.local>
2017-06-12 08:05:28 -0700

    month difference formula

--- ryan_tools/__init__.py
+++ ryan_tools/__init__.py
@@ -28,6 +28,9 @@ def get_index(findme, whatever_list ):
         iterator = iterator + 1
     return False
 
+def diff_month(d1, d2):
+    return (d1.year - d2.year) * 12 + d1.month - d2.month
+
 def rearrange_dataframe_columns(dataframe, order):
     '''Takes a dataframe and a column order, the column order is litteraly the column names ordered correctly
     IF you want to have it reversed, used python's built in reversed() function on <order>'''
