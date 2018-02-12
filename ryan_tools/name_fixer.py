5421f0e578d3c451766bd8e856d1e8a2991b8247
ryan <2yan@outlook.com>
2018-02-12 15:49:12 -0800

    name capitalization

--- ryan_tools/name_fixer.py
+++ ryan_tools/name_fixer.py
@@ -5,7 +5,7 @@ def gl(text, loc):
         return text[loc]
     except IndexError:
         return ''
-def is_seperator(thing, seperators = [' ','', '.', '\'']):
+def is_seperator(thing, seperators = [' ','', '.', '\'', '-', '`', '\'']):
     
     if thing in seperators:
         return True
@@ -24,7 +24,7 @@ def vowel(letter):
         return True
     return False
 
-def fix_human_name(text):
+def fix_name(text):
     if text.upper() != text and text.lower() != text:
         return text
 
@@ -44,6 +44,9 @@ def fix_human_name(text):
         
         if is_seperator(letter, ['', ' ']) and is_seperator(gl(done_text, i -3)) and not (vowel(gl(done_text, i - 1)) or vowel(gl(done_text, i - 2))): 
             done_text = capitalize(done_text, [i - 1, i - 2])
+        
+        #if (gl(done_text, i-3) == 'M') and (gl(done_text, i-2) == 'a') and (gl(done_text, i-1) == 'c'):
+        #    letter = letter.upper()        
         
         done_text = done_text + letter
     done_text =  done_text[1: len(done_text)]
