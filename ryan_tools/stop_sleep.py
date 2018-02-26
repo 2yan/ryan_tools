81ad9e88558591cb42c74a231900da9691fc5b02
ryan <2yan@outlook.com>
2018-02-26 11:08:48 -0800

    added windows check

--- ryan_tools/stop_sleep.py
+++ ryan_tools/stop_sleep.py
@@ -5,15 +5,19 @@ API documentation:
 https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx'''
 ES_CONTINUOUS = 0x80000000
 ES_SYSTEM_REQUIRED = 0x00000001
-
+import os
 
 def stop_sleep():
+    assert(os.name == 'nt')
     import ctypes
     ctypes.windll.kernel32.SetThreadExecutionState(
         ES_CONTINUOUS | \
         ES_SYSTEM_REQUIRED)
+    return 'stopping sleep'
 
 def allow_sleep():
+    assert(os.name == 'nt')
     import ctypes
     ctypes.windll.kernel32.SetThreadExecutionState(
         ES_CONTINUOUS)
+    return 'allowing sleep'
