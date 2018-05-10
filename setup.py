eefa83de8152d5b2537db742b9e16b599a0ec53f
ryan <2yan@outlook.com>
2018-05-10 15:11:17 -0700

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ryan_tools',
-      version='0.322',
+      version='0.34',
       description='ryan_tools for easy setup',
       url='https://github.com/2yan/ryan_tools',
       author='Ryan Francis',
